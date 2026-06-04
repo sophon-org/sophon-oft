a9e5e2b8a5ed239ff1b6e0d717860176845dd746
CryptoMan0x <156699519+CryptoMan0x@users.noreply.github.com>
2026-06-04 13:23:12 -0400

    Added SophonTokenOFTAdapter (ERC20); renamed old file to SophonTokenNativeOFTAdapter

--- contracts/mocks/MyOFTAdapterMock.sol
+++ contracts/mocks/MyOFTAdapterMock.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { SophonTokenOFTAdapter } from "../SophonTokenOFTAdapter.sol";
+import { SophonTokenNativeOFTAdapter } from "../SophonTokenNativeOFTAdapter.sol";
 
 // @dev WARNING: This is for testing purposes only
-contract MyOFTAdapterMock is SophonTokenOFTAdapter {
-    constructor(address _lzEndpoint, address _delegate) SophonTokenOFTAdapter(_lzEndpoint, _delegate) {}
+contract MyOFTAdapterMock is SophonTokenNativeOFTAdapter {
+    constructor(address _lzEndpoint, address _delegate) SophonTokenNativeOFTAdapter(_lzEndpoint, _delegate) {}
 }
